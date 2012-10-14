4641e99f919047daa0744217264487da73cc69e6
Nick Gudushauri <nick.gudushauri@azry.com>
2012-10-14 10:40:12 +0400

    test main class

--- src/com/azry/test/Main.java
+++ src/com/azry/test/Main.java
@@ -9,6 +9,6 @@ package com.azry.test;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello GIT world!");
     }
 }
