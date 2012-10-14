9957d25e8dfe2dbbf6a7d4ca436d8ea3bfc0bb2f
Nick Gudushauri <nick.gudushauri@azry.com>
2012-10-14 10:56:42 +0400

    test main class

--- src/com/azry/test/Main.java
+++ src/com/azry/test/Main.java
@@ -1,12 +1,5 @@
 package com.azry.test;
 
-/**
- * Created with IntelliJ IDEA.
- * User: nikoloz
- * Date: 10/14/12
- * Time: 10:25 AM
- * To change this template use File | Settings | File Templates.
- */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GIT world!");
