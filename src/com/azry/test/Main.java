4d9f00b420491d1e647162173c9fa45f73b0ac5e
Nick Gudushauri <nick.gudushauri@azry.com>
2012-10-14 11:01:01 +0400

    minor changes

--- src/com/azry/test/Main.java
+++ src/com/azry/test/Main.java
@@ -2,6 +2,6 @@ package com.azry.test;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello GIT world!");
+        System.out.println("Hello GIT world!!!");
     }
 }
