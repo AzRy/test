e647db3375605a08e17a59241fa4138179f72482
Nick Gudushauri <nick.gudushauri@azry.com>
2012-10-14 11:11:52 +0400

    Added other print

--- src/com/azry/test/Main.java
+++ src/com/azry/test/Main.java
@@ -3,5 +3,6 @@ package com.azry.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GIT world!!!");
+        System.out.println("Another hello!");
     }
 }
